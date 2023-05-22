bad3393eca33ec068890b7f9dfb36fd97014e8b0
rockdaworld <sjh002@ucsd.edu>
2023-05-21 18:58:18 -0700

    Changes to index and adding uuid to packages

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,9 +1,11 @@
 /* Use this file to expose your routes */
 const express = require('express')
-const products = require('./routes/example_route.js')
+const products = require('./routes/list_route.js')
 const app = express()
 
 // Using route '/product' with everything from products, our example route
-app.use("/product", products)
+
+app.use(express.json());
+app.use("/", products)
 
 module.exports = app
