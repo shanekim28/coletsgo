5ce9b82a00031990ab7fc2981fc133ffbed35772
Shane Kim <shane.kim@epitone.ai>
2023-05-24 11:35:17 -0700

    Integrate frontend together

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,10 +1,15 @@
 import ListViewComponent from "../components/ListViewComponent";
+import CheckInComponent from "../components/CheckInComponent";
 import MainLayoutModule from "./MainLayout.module.css";
 
 const MainLayout = () => {
   return (
     <div className={MainLayoutModule.mainContent}>
       <ListViewComponent />
+
+      <div className={MainLayoutModule.button}>
+        <CheckInComponent />
+      </div>
     </div>
   )
 }
