ef3d208817ee754c2aadf52b6f0835db78f8de3a
Shane Kim <shane.kim@epitone.ai>
2023-05-24 23:12:34 -0700

    Hotfix ssl

--- backend/context/dbcontext.js
+++ backend/context/dbcontext.js
@@ -1,7 +1,7 @@
 const { dbuser, dbpass, db, dbhost, dbport } = require('../config');
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(`postgres://${dbuser}:${dbpass}@${dbhost}:${dbport}/${db}`, {
+const sequelize = new Sequelize(`postgres://${dbuser}:${dbpass}@${dbhost}:${dbport}/${db}?ssl=true`, {
   logging: console.log,
 });
 
